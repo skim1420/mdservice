7d93f21911c442c3bc6aa4705f7d8493659f956d
Steven Kim <stevenkim@google.com>
2017-10-03 17:18:38 +0000

    purple

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "<body style='background-color: blue'><h1>Hello World!</h1></body>")
+	io.WriteString(w, "<body style='background-color: purple'><h1>Hello World!</h1></body>")
 }
 
 func main() {
